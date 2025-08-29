0dbcd969935dffaf3cef6a00931b7aecd5355132
stephanjames <stephanjames055@gmail.com>
2025-08-29 00:59:12 +0000

    updated variable files

--- staging/variables.tf
+++ staging/variables.tf
@@ -5,10 +5,13 @@ variable "aws_region" {
 
 variable "s3_bucket" {
   description = "Nom du bucket S3 pour staging"
+  default     = "mon-bucket-staging"
   type        = string
+  
 }
 
 variable "cloudfront_distribution_id" {
   description = "ID de la distribution CloudFront pour staging"
+  default     = "XXXXXXXXXXXX"
   type        = string
 }
